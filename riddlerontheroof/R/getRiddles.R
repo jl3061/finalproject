71133aa32c9ba9f4fa8c07d6c79308bb6517da8c
jl3061 <jl3061@georgetown.edu>
2023-10-26 18:26:49 -0400

    edit

--- riddlerontheroof/R/getRiddles.R
+++ riddlerontheroof/R/getRiddles.R
@@ -1,12 +1,12 @@
 #' @title playRiddles
 #' This function allows you to add the riddles to your environment.
 #' Riddles from https://github.com/crawsome/PyRPG_Mini.git
-#' @param file_path File path for preloaded riddles excel/csv file.
+#' @param file_path File path for preloaded riddles rda file.
 #' @export
 #' @examples
 #' riddles <- getRiddles()
 
-getRiddles <- function(file_path = "data/riddles.csv") {
-  riddles <- read.csv(file_path)
+getRiddles <- function() {
+  data("riddles", package = "riddlerontheroof")
   return(riddles)
 }
